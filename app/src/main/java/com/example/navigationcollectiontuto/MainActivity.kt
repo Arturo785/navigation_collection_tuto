b758cd2935dbf5345aad39d785d8c51f1e2dc008
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2020-10-19 13:48:00 -0500

    Addition of navGraph, navHost, dayNight theme and material components for UI

--- app/src/main/java/com/example/navigationcollectiontuto/MainActivity.kt
+++ app/src/main/java/com/example/navigationcollectiontuto/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.navigationcollectiontuto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //This is just because DayNight theme uses the darkMode of the system and
+        //Because I don't want to configure the theme I just made it lightTheme
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
