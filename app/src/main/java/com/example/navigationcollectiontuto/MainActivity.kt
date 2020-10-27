2bd0a4ae539cc00145c60a6f7c0da12c039bc944
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2020-10-27 14:52:17 -0600

    SetUp with the toolBar and with custom labels

--- app/src/main/java/com/example/navigationcollectiontuto/MainActivity.kt
+++ app/src/main/java/com/example/navigationcollectiontuto/MainActivity.kt
@@ -3,14 +3,38 @@ package com.example.navigationcollectiontuto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navControler : NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //This is just because DayNight theme uses the darkMode of the system and
         //Because I don't want to configure the theme I just made it lightTheme
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+
+        //This is needed for the toolBar management
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navControler = navHostFragment.findNavController()
+
+        setSupportActionBar(toolBar)
+        setupActionBarWithNavController(navControler)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navControler.navigateUp() || super.onSupportNavigateUp()
     }
 }
